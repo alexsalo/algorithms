385cf57eb1994f274a637dc1e94aea9dfdc471bc
salo <alexsalovrn@gmail.com>
2015-01-23 15:54:57 -0600

    merge-test

--- mergesort.py
+++ mergesort.py
@@ -1,1 +1,26 @@
 __author__ = 'alex'
+
+def merge(l, r):
+    result = []
+    i = j = 0
+    while i < len(l) and j < len(r):
+        if l[i] < r[j]:
+            result.append(l[i])
+            i += 1
+        else:
+            result.append(r[j])
+            j += 1
+    result += l[i:]
+    result += r[j:]
+    return result
+
+def msort(a):
+    if len(a) == 1:
+        return a
+    mid = len(a) / 2
+    return merge(msort(a[:mid]), msort(a[mid:]))
+
+
+array = [101, 50, 2, 43, 6, 100, 8, 20, 3, 15]
+print array
+print msort(array)
