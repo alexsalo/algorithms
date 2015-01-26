3b520b7865a4e072dc4c7888b8abfb2875efbe8b
salo <alexsalovrn@gmail.com>
2015-01-26 15:39:40 -0600

    quick sort

---
+++ quicksort.py
@@ -0,0 +1,15 @@
+__author__ = 'alex'
+
+def qsort(arr):
+    print arr
+    if len(arr) < 2:
+        return arr
+    pivots = [x for x in arr if x == arr[0]]
+    lo = qsort([x for x in arr if x < arr[0]])
+    hi = qsort([x for x in arr if x > arr[0]])
+
+    return lo + pivots + hi
+
+
+array = [11, 50, 2, 43, 6, 100, 8, 20, 3, 15]
+print qsort(array)
